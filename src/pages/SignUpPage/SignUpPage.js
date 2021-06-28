1315cc7ba8b0163f05b74e6d1b6b322b4f0c6ba4
Anderson Oliveira <tandersonf@hotmail.com>
2021-06-28 16:53:50 -0300

    logica do checkpassword adicionada parcialmente

--- src/pages/SignUpPage/SignUpPage.js
+++ src/pages/SignUpPage/SignUpPage.js
@@ -5,10 +5,35 @@ function SignUpPage(params) {
     const [form, onChange] = useForm({ name: "", nickname: "", email: "", password: "" })
     const [formPassword, onChangeConfirmPassword] = useForm({ confirmPassword: "" })
 
+
     useEffect(() => {
-        // console.log('form  detrno do useEffect:',form)
-        // console.log('formPassword  detrno do useEffect:',formPassword)
-    }, )
+
+    },[form.password])
+    const checkFormPassword = event => {
+        onChangeConfirmPassword(event)
+        // console.log('to no EVENT DENTRO DO CHECK FORM.... qual e o EVENTO: ',event.target.value)
+        // console.log(` form.password: ,${form.password} .... formPassword.confirmPassword: ${formPassword.confirmPassword}`)
+        if (event.target.value.length > 5 && form.password.length > 5 && event.target.value !== form.password) {
+            console.log('ta diferente , vem com a gente....')
+        } else {
+            console.log('PARABENS, pode prosseguir...')
+        }
+    }
+
+    const checkMainPassword = event => {
+        // console.log(` form.password: ,${form.password} .... formPassword.confirmPassword: ${formPassword.confirmPassword}`)
+        onChange(event)
+        if (event.target.value.length > 5 && formPassword.confirmPassword.length > 5 && event.target.value !== formPassword.checkFormPassword) {
+            console.log('ta diferente , vem com a gente....')
+        } else {
+            console.log('PARABENS, pode prosseguir...')
+        }
+    }
+
+    useEffect(() => {
+        console.log('form  dentro do useEffect:',form)
+        console.log('formPassword  detrno do useEffect:',formPassword)
+    })
     return (
         <>
             <h1>MainPage</h1>
@@ -16,8 +41,8 @@ function SignUpPage(params) {
                 <input name="name" onChange={onChange} placeholder="name" value={form.name}></input>
                 <input name="email" onChange={onChange} placeholder="email" value={form.email}></input>
                 <input name="nickname" onChange={onChange} placeholder="nickname" value={form.nickname}></input>
-                <input name="password" onChange={onChange} placeholder="password" value={form.password}></input>
-                <input name="confirmPassword" onChange={onChangeConfirmPassword} placeholder="confirmPassword" value={formPassword.confirmPassword}></input>
+                <input name="password" onChange={checkMainPassword} placeholder="password" value={form.password}></input>
+                <input name="confirmPassword" onChange={checkFormPassword} placeholder="confirmPassword" value={formPassword.confirmPassword}></input>
             </form>
 
         </>
